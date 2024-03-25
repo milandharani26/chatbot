9c338cdda0d6deec01481d1a9211fe9b62eb966c
milandharani26 <113498278+milandharani26@users.noreply.github.com>
2024-03-25 17:59:16 +0530

    initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,5 +17,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
+    "no-unused-vars": "warn",
+    'react/jsx-key': 'warn',
   },
 }
